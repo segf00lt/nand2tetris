d72c708e23a6351aa94071d60d1034f22e2b4bef
segf00lt <joaodear2007@gmail.com>
2022-11-11 12:05:34 -0300

    fixed return command

--- projects/07/hackvm.c
+++ projects/07/hackvm.c
@@ -316,15 +316,15 @@ void compile(char *fname, char *text, FILE *dest) {
 			break;
 		case RET:
 			fprintf(dest,
-					"@LCL\nD=M\n@R5\nM=D\n"
-					"@5\nA=D-A\nD=M\n@R6\nM=D\n"
+					"@LCL\nD=M\n@R13\nM=D\n"
+					"@5\nA=D-A\nD=M\n@R14\nM=D\n"
 					"@SP\nA=M-1\nD=M\n@ARG\nA=M\nM=D\n"
 					"@ARG\nD=M+1\n@SP\nM=D\n"
-					"@R5\nM=M-1\nA=M\nD=M\n@THAT\nM=D\n"
-					"@R5\nM=M-1\nA=M\nD=M\n@THIS\nM=D\n"
-					"@R5\nM=M-1\nA=M\nD=M\n@ARG\nM=D\n"
-					"@R5\nM=M-1\nA=M\nD=M\n@LCL\nM=D\n"
-					"@R6\nA=M\n0;JMP\n");
+					"@R13\nM=M-1\nA=M\nD=M\n@THAT\nM=D\n"
+					"@R13\nM=M-1\nA=M\nD=M\n@THIS\nM=D\n"
+					"@R13\nM=M-1\nA=M\nD=M\n@ARG\nM=D\n"
+					"@R13\nM=M-1\nA=M\nD=M\n@LCL\nM=D\n"
+					"@R14\nA=M\n0;JMP\n");
 			break;
 		}
 	}
